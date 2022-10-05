717c74c43d21c21489d0218764571a9ad042f378
dejuaun <dejuaun@gmail.com>
2022-10-05 18:15:43 +0100

    css

--- helloWorld.php
+++ helloWorld.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>Hello World PHP</title>
+	<link rel="stylesheet" type="text/css" href="helloWorld.css">
 </head>
 <body>
 <h2>Varibles and their Types</h2>
@@ -37,23 +38,108 @@
 echo DATE("y.m.d")."<br>";
 ?>
 <br>
-<br>
 <?php 
 	$vari1=1;
 	echo $vari1;
 ?>
-<hr>
 <br>
+<hr>
 <h2>Echo and Print Statements</h2>
 <?php
 	echo "hello world without parenthesis"."<br>";
 	echo ("hello world with parenthesis")."<br>";
 ?>
-<hr>
 <br>
+<hr>
 <h2>Die and Exit functions</h2>
 <?php 
+	$website = "https://www.google.com";
+	fopen($website, "r")
+		or die("you are unable to connect to the $website");
+?>
+<?php 
+	$website = "https://www.google.com";
+	fopen($website, "r")
+		or exit("you are unable to connect to the $website");
+?>
+<br>
+<hr>
+<h2>basic data types in PHP</h2>
+<?php 
+	$integer = 123;
+	var_dump($integer);
+	echo "<br>";
+
+	$float = 123.123;
+	var_dump($float);
+	echo "<br>";
+
+	$string = "This is a string";
+	var_dump($string);
+	echo "<br>";
+
+	$boolean = true;
+	var_dump($boolean);
+?>
+<br>
+<hr>
+<h2>PHP Constants</h2>
+<?php 
+	define("GREETING","Hello you! How are you today?");
+echo constant("GREETING")."<br>";
+ 	define(PI, 3.14);
+ 	echo (PI)."<br>";
+ 	echo "This is a cirle radius value which is ","", 2*PI*5;
+ 	echo ("<br>");
+
+ 	const XYZ="This is the const keyword";
+ 	echo XYZ;
+?>
+<br>
+<hr>
+<h2>Arithmetic Operators</h2>
+<?php 
+	$a = 20;
+	$b = 50;
+	echo($a - $b) ."= &nbsp 20 - 50", "<br>";
+	echo($a + $b) ."= &nbsp 20 + 50", "<br>";
+	echo($a * $b) ."= &nbsp 20 x 50", "<br>";
+	echo($a / $b) ."= &nbsp 20 ÷ 50", "<br>";
+	echo($a % $b) ."= &nbsp 20 ÷ 50 remainder", "<br>";
+	echo $a;
 	
+
 ?>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
